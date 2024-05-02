e42b7012e55e73fc9dc4ffd4fa911d3a73e87fb8
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-05-02 16:30:56 -0600

    Create quotient_reminder.cpp
    This program takes in 2 variables from the user and displays the quotient and the reminder when int 1 is divided by int 2

---
+++ chapter_1/quotient_reminder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 0;
+    int y = 0;
+    
+    cout << "Enter the first variable: ";
+    cin >> x;
+    
+    cout << "Enter the second variable: ";
+    cin >> y;
+    
+    cout << x / y << "\n";
+    cout << x % y;
+}
