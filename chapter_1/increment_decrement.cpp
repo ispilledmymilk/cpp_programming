d7897439284ecd1bc792a27e4bd25c3c834d7633
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2024-05-02 17:10:48 -0600

    Create increment_decrement.cpp
    This program shows the use of increment and decrement operators in C++.

---
+++ chapter_1/increment_decrement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 1, m = 8;
+    cout << "n is " << n << " m is " << m << "\n";
+    n++;
+    m--;
+    //after the increment and decrement operators.
+    cout << "n is " << n << " m is " << m << "\n";
+    
+    return 0;
+}
